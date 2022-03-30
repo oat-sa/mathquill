6e614e85570497224b8d90a10db6363d3ed220ce
Ashim Bayashev <intelingelse@gmail.com>
2022-03-30 10:43:37 +0300

    added feature of the bottom notation to the lim function

--- src/commands/math/commands.js
+++ src/commands/math/commands.js
@@ -361,6 +361,60 @@ LatexCmds['^'] = P(SupSub, function(_, super_) {
   };
 });
 
+var BottomNotation = P(MathCommand, function (_, super_){
+  _.init = function (ch, html) {
+    var htmlTemplate =
+        '<span class="mq-regular-operator mq-non-leaf">'
+        +   '<span>'+html+'</span>'
+        +   '<span class="mq-from">&0</span>'
+        + '</span>'
+    ;
+
+    Symbol.prototype.init.call(this, ch, htmlTemplate);
+  };
+
+  _.createLeftOf = function(cursor) {
+    super_.createLeftOf.apply(this, arguments);
+    if (cursor.options.sumStartsWithNEquals) {
+      Letter('n').createLeftOf(cursor);
+      Equality().createLeftOf(cursor);
+    }
+  };
+  _.latex = function() {
+    function simplify(latex) {
+      return latex.length === 1 ? latex : '{' + (latex || ' ') + '}';
+    }
+    return this.ctrlSeq + '_' + simplify(this.ends[L].latex());
+  };
+  _.parser = function() {
+    var string = Parser.string;
+    var optWhitespace = Parser.optWhitespace;
+    var succeed = Parser.succeed;
+    var block = latexMathParser.block;
+
+    var self = this;
+    var blocks = self.blocks = [ MathBlock(), MathBlock() ];
+    for (var i = 0; i < blocks.length; i += 1) {
+      blocks[i].adopt(self, self.ends[R], 0);
+    }
+
+    return optWhitespace.then(string('_').or(string('^'))).then(function(supOrSub) {
+      var child = blocks[supOrSub === '_' ? 0 : 1];
+      return block.then(function(block) {
+        block.children().adopt(child, child.ends[R], 0);
+        return succeed(self);
+      });
+    }).many().result(self);
+  };
+  _.finalizeTree = function() {
+    this.downInto = this.ends[L];
+    this.ends[L].upOutOf = this.ends[R];
+  };
+
+});
+
+LatexCmds.lim = bind(BottomNotation, '\\lim', 'lim');
+
 var SummationNotation = P(MathCommand, function(_, super_) {
   _.init = function(ch, html) {
     var htmlTemplate =
